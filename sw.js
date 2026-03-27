936332b70faba5295c20292df48885f0361b72f9
William Nevits <wnevits@gmail.com>
2026-03-26 23:31:14 -0400

    Automated Update - Version 1774582274

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'brew-lab-v1774582093';
+const CACHE_NAME = 'brew-lab-v1774582274';
 const ASSETS = ['index.html', 'mobile_recipes.js', 'brewlab_icon.png'];
 
 self.addEventListener('install', (e) => {
